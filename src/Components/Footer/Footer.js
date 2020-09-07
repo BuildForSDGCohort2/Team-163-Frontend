43a2092f995f98849215d99385a8ed26175bb406
Fadhil <fadhil.akins@gmail.com>
2020-09-07 15:05:16 +0100

    router added

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {FooterDiv} from '../StyledComponents'
+
+const Footer =()=>{
+	return (
+<FooterDiv><p>&#169; Copyright 2020</p></FooterDiv>
+		)
+}
+
+export default Footer;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import {NavLinks} from './Components/StyledComponents'
+import {Navs, Navlist, SButton} from '../StyledComponents'
 
 const Navbar = ()=>{
 	return(
-	<div>
-	<nav>
-	    <a href="1">ROADABLE</a>
-	    <NavLinks>
-		    <a href="1">HOME</a>
-		    <a href="2">HOW IT WORKS</a>
-		    <a href="3">ABOUT US</a>
-	    </NavLinks>
-	    <a href="4">LOGIN</a>
-	    <a href="5">SIGN UP</a>
-	</nav>
-	</div>)
+	<Navs>
+	<a className="logo" href="1">ROADABLE</a>
+      <Navlist>
+         <li><a href="2">HOME</a></li>
+         <li><a href="3">HOW IT WORKS</a></li>
+         <li><a href="3">ABOUT US</a></li>
+      </Navlist>
+      <Navlist access>
+        <li href="4"><SButton>LOGIN</SButton></li>
+        <li href="5"><SButton>SIGN UP</SButton></li>
+      </Navlist>
+	</Navs>)
 }
+
+export default Navbar;
